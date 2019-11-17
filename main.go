59b5ba2a5f443d7ace9d35f74968916f0547959d
Donnie Tolbert <donniet@gmail.com>
2019-11-17 09:39:01 -0600

    infinite wait somewhere

--- main.go
+++ main.go
@@ -2,19 +2,66 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"os"
+	"os/signal"
+	"runtime/debug"
 
 	"github.com/donniet/ilclient"
 )
 
 func main() {
+	sig := make(chan os.Signal)
+	signal.Notify(sig, os.Interrupt)
+
+	defer func() {
+		if r := recover(); r != nil {
+			debug.PrintStack()
+			os.Exit(1)
+		}
+	}()
+
+	go func() {
+		<-sig
+
+		debug.PrintStack()
+		panic(fmt.Errorf("interrupted"))
+	}()
+
 	client := ilclient.New()
 
-	cam, err := client.NewComponent("camera")
-
+	cam, err := client.NewComponent("image_encode",
+		ilclient.CreateFlagDisableAllPorts,
+		ilclient.CreateFlagEnableOutputBuffers,
+		ilclient.CreateFlagEnableInputBuffers)
 	if err != nil {
-		fmt.Printf("error: %v", err)
+		log.Printf("error: %v", err)
 		return
 	}
+	state, err := cam.State()
+	if err != nil {
+		log.Printf("error getting state: %v", err)
+		return
+	}
+	log.Printf("camera state: %v", state)
 
-	fmt.Printf("client: %v", cam)
+	cam.SetState(ilclient.StateIdle)
+
+	state, err = cam.State()
+	if err != nil {
+		log.Printf("error getting state: %v", err)
+		return
+	}
+	log.Printf("camera state: %v", state)
+
+	enc, err := client.NewComponent("video_encode",
+		ilclient.CreateFlagEnableInputBuffers,
+		ilclient.CreateFlagEnableOutputBuffers,
+		ilclient.CreateFlagDisableAllPorts)
+	if err != nil {
+		log.Printf("error: %v", err)
+	}
+
+	log.Printf("cam: %v", cam)
+	log.Printf("enc: %v", enc)
 }
